d892d2adc2d17a8d73e6aaa4650d1df9d00ecefd
intptrr <dev@intptrr.cc>
2026-05-31 07:57:41 -0700

    feat(nvim): add common keymaps and quality-of-life options

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,39 @@
 local map = vim.keymap.set
+
+-- file operations
+map("n", "<leader>w", "<cmd>write<CR>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>quit<CR>", { desc = "Quit window" })
+
+-- clear search highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- common windows-style shortcuts
+map("n", "<C-s>", "<cmd>write<CR>", { desc = "Save file" })
+map("v", "<C-c>", '"+y', { desc = "Copy selection to clipboard" })
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+-- window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- buffer navigation
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+
+-- move lines up/down
+map("n", "<A-j>", "<cmd>move .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", "<cmd>move .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":move '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":move '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- keep selection after indenting
+map("v", "<", "<gv", { desc = "Indent left and reselect" })
+map("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- keep cursor centered when scrolling and searching
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+map("n", "n", "nzzzv", { desc = "Next search result and center" })
+map("n", "N", "Nzzzv", { desc = "Previous search result and center" })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,6 +6,11 @@ vim.g.maplocalleader = "\\"
 
 local opt = vim.opt
 
+-- ui related
+opt.termguicolors = true -- enable 24-bit RGB colors
+opt.mouse = "a" -- enable mouse support in all modes
+opt.signcolumn = "yes" -- always show the sign column to avoid text shifting
+
 -- cursor related
 opt.ruler = true -- show cursor line number and position on the status bar
 opt.guicursor = "n-v-c-i:block" -- change cursor to block for all modes
@@ -20,3 +25,14 @@ opt.shiftround = true -- round the indent
 opt.tabstop = 2 -- number of spaces in the tab
 opt.shiftwidth = 2 -- size of a indent
 
+-- search related
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- override ignorecase if search contains uppercase
+opt.hlsearch = true -- highlight all search matches
+opt.incsearch = true -- show matches incrementally as you type
+
+-- completion related
+opt.completeopt = { "menuone", "noselect" } -- show menu even for one match, don't auto-select
+
+-- clipboard related
+opt.clipboard = "unnamedplus" -- use the system clipboard for all yank/delete/paste operations
